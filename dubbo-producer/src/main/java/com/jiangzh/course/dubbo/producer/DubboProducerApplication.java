05c5c44810c781a16993be89e1c7f446276fcbce
jiangzheng <jz292679@sina.com>
2021-07-07 22:36:58 +0800

    集成Dubbo配置文件

--- dubbo-producer/src/main/java/com/jiangzh/course/dubbo/producer/DubboProducerApplication.java
+++ dubbo-producer/src/main/java/com/jiangzh/course/dubbo/producer/DubboProducerApplication.java
@@ -2,8 +2,10 @@ package com.jiangzh.course.dubbo.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(locations = {"classpath:applicationContext-dubbo.xml"})
 public class DubboProducerApplication {
 
     public static void main(String[] args) {
