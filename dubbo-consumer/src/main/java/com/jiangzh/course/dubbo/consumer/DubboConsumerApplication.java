0578ea10c7323ff7f92405af5cd9c73ec93a3caa
jiangzheng <jz292679@sina.com>
2021-07-07 22:52:32 +0800

    日志文件集成 + provider和consumer测试

--- dubbo-consumer/src/main/java/com/jiangzh/course/dubbo/consumer/DubboConsumerApplication.java
+++ dubbo-consumer/src/main/java/com/jiangzh/course/dubbo/consumer/DubboConsumerApplication.java
@@ -1,7 +1,9 @@
 package com.jiangzh.course.dubbo.consumer;
 
+import com.jiangzh.course.dubbo.service.HelloServiceAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
@@ -9,7 +11,13 @@ import org.springframework.context.annotation.ImportResource;
 public class DubboConsumerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DubboConsumerApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(DubboConsumerApplication.class, args);
+
+        HelloServiceAPI producerService = context.getBean("producerService", HelloServiceAPI.class);
+
+        String message = "I'm Jiangzh";
+        System.out.println(producerService.sayHello(message));
     }
 
 }
